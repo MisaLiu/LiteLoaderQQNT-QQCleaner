c8adf71ae29d972d697481d9b0b6bfac978ab2fc
Misa Liu <misaliu@misaliu.top>
2024-02-28 17:23:26 +0800

    chore: Add ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        'src/**/*.ts',
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-multi-spaces': 2,
+    'no-trailing-spaces': 2,
+    'space-before-function-paren': [2, 'never'],
+  }
+};
