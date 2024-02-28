873f832362d34fa9db196a102b1098330ed4fd5f
Misa Liu <misaliu@misaliu.top>
2024-02-28 18:25:55 +0800

    feat(renderer/components/item): Add `subtitleId`

--- src/renderer/components/item.ts
+++ src/renderer/components/item.ts
@@ -1,9 +1,9 @@
 
-export const SettingItem = (title: string, subtitle?: string, action?: string) => {
+export const SettingItem = (title: string, subtitle?: string, action?: string, subtitleId?: string) => {
   return `<setting-item>
   <div>
     <setting-text>${title}</setting-text>
-    ${subtitle ? `<setting-text data-type="secondary">${subtitle}</setting-text>` : ''}
+    ${subtitle ? `<setting-text data-type="secondary" ${subtitleId ? `id="${subtitleId}"` : ''}>${subtitle}</setting-text>` : ''}
   </div>
   ${action ? action : ''}
 </setting-item>`;
