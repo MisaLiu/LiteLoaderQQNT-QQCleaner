79abdf0ff33151fee25d3ca7bff3afcb913d7a70
Misa Liu <misaliu@misaliu.top>
2024-02-29 17:47:28 +0800

    chore: Change Vite's base config

--- config/base.config.ts
+++ config/base.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: resolve(__dirname, '../'),
   resolve: {
     alias: {
       '@': resolve(__dirname, '../src'),
