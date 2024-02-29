cea918d5ee42a8f9e9cca78fda825a989421b7d5
Misa Liu <misaliu@misaliu.top>
2024-02-29 17:27:39 +0800

    feat(common/utils): Add plugin info

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,10 +1,15 @@
 import * as path from 'path';
+import { version as PackageVersion, liteloader_manifest as PluginInfo } from '@/../package.json';
 import { ConfigUtil } from './config';
 
-export const CONFIG_DIR = path.resolve(global.LiteLoader.plugins['LLQQCleaner'].path.data, 'config.json');
+export const PLUGIN_SLUG = PluginInfo.slug;
+export const PLUGIN_NAME = PluginInfo.name;
+export const PLUGIN_VERSION = PackageVersion;
+
+export const PLUGIN_CONFIG_DIR = path.resolve(global.LiteLoader.plugins['LLQQCleaner'].path.data, 'config.json');
 
 export function getConfigUtil() {
-  return new ConfigUtil(CONFIG_DIR);
+  return new ConfigUtil(PLUGIN_CONFIG_DIR);
 }
 
 export function printLog(...args) {
