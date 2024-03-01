d52e441fd60d0cba99902a64b9af05fa22b70335
Misa Liu <misaliu@misaliu.top>
2024-03-01 23:42:14 +0800

    feat(main/cleaner): Add clock regist log

--- src/main/cleaner/index.ts
+++ src/main/cleaner/index.ts
@@ -9,5 +9,8 @@ export function InitCleaner() {
     log('Startup clean will be start in 5s...');
     setTimeout(() => runCleaner(config), 5000);
   }
-  if (config.cleanClock) setInterval(() => runCleaner(config), config.cleanClockInterval * 60 * 60 * 1000);
+  if (config.cleanClock) {
+    setInterval(() => runCleaner(config), config.cleanClockInterval * 60 * 60 * 1000);
+    log(`Clean clock registed, cleaning will be start every ${config.cleanCacheAfterDaysNumber} hours.`);
+  }
 }
