95b673dce2fbcdebd0824ff0b5ad3c9f63999e5f
Misa Liu <misaliu@misaliu.top>
2024-08-08 15:53:45 +0800

    chore: Fix typing issues

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,81 +1,75 @@
-import { IPluginConfig, IPluginStatsExtend } from '@/common/utils/types';
+import type { LLQQCleaner as ILLQQCleaner } from './preload';
 
-declare module '*?raw' {
-  const content: string;
-  export default content;
+declare global {
+  const LiteLoader: ILiteLoader;
+  const LLQQCleaner: typeof ILLQQCleaner;
 }
 
-declare namespace LLQQCleaner {
-  const getConfig: () => Promise<IPluginConfig>;
-  const setConfig: (config: IPluginConfig) => Promise<void>;
-  const getStats: () => Promise<IPluginStatsExtend>;
-}
-
-declare namespace LiteLoader {
-  const path: ILiteLoaderPath;
-  const versions: ILiteLoaderVersion;
-  const os: ILiteLoaderOS;
-  const package: ILiteLoaderPackage;
-  const config: {
+declare interface ILiteLoader {
+  path: ILiteLoaderPath;
+  versions: ILiteLoaderVersion;
+  os: ILiteLoaderOS;
+  package: ILiteLoaderPackage;
+  config: {
     LiteLoader: {
       disabled_plugins: string[],
     }
   };
-  const plugins: Record<string, ILiteLoaderPlugin>;
-  const api: ILiteLoaderAPI;
+  plugins: Record<string, ILiteLoaderPlugin>;
+  api: ILiteLoaderAPI;
+}
 
-  interface ILiteLoaderPath {
-    root: string,
-    profile: string,
-    data: string,
-    plugins: string,
-  }
+interface ILiteLoaderPath {
+  root: string,
+  profile: string,
+  data: string,
+  plugins: string,
+}
 
-  interface ILiteLoaderVersion {
-    qqnt: string,
-    liteloader: string,
-    node: string,
-    chrome: string,
-    electron: string,
-  }
+interface ILiteLoaderVersion {
+  qqnt: string,
+  liteloader: string,
+  node: string,
+  chrome: string,
+  electron: string,
+}
 
-  interface ILiteLoaderOS {
-    platform: 'win32' | 'linux' | 'darwin',
-  }
+interface ILiteLoaderOS {
+  platform: 'win32' | 'linux' | 'darwin',
+}
 
-  interface ILiteLoaderPackage {
-    liteloader: object,
-    qqnt: object,
-  }
+interface ILiteLoaderPackage {
+  liteloader: object,
+  qqnt: object,
+}
 
-  interface ILiteLoaderPlugin {
-    manifest: object,
-    incompatible: boolean,
-    disabled: boolean,
-    path: ILiteLoaderPluginPath
-  }
+interface ILiteLoaderPlugin {
+  manifest: object,
+  incompatible: boolean,
+  disabled: boolean,
+  path: ILiteLoaderPluginPath
+}
 
-  interface ILiteLoaderPluginPath {
-    plugin: string,
-    data: string,
-    injects: ILiteLoaderPluginPathInject
-  }
+interface ILiteLoaderPluginPath {
+  plugin: string,
+  data: string,
+  injects: ILiteLoaderPluginPathInject
+}
 
-  interface ILiteLoaderPluginPathInject {
-    main: string,
-    renderer: string,
-    preload: string,
-  }
+interface ILiteLoaderPluginPathInject {
+  main: string,
+  renderer: string,
+  preload: string,
+}
 
-  interface ILiteLoaderAPI {
-    openPath: (path: string) => void,
-    openExternal: (url: string) => void,
-    disablePlugin: (slug: string) => void,
-    config: ILiteLoaderAPIConfig,
-  }
+interface ILiteLoaderAPI {
+  openPath: (path: string) => void,
+  openExternal: (url: string) => void,
+  disablePlugin: (slug: string) => void,
+  config: ILiteLoaderAPIConfig,
+}
 
-  interface ILiteLoaderAPIConfig {
-    set: (slug: string, new_config: object) => unknown,
-    get: (slug: string, default_config?: object) => object,
-  }
+interface ILiteLoaderAPIConfig {
+  set: (slug: string, new_config: object) => unknown,
+  get: (slug: string, default_config?: object) => object,
 }
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,19 +1,20 @@
 import { contextBridge } from 'electron';
 import { ipcRenderer } from 'electron';
 import { EIPCChannel } from '@/common/channels';
-import { IPluginConfig } from '@/common/utils/types';
+import { IPluginConfig, IPluginStatsExtend } from '@/common/utils/types';
 
 const LLQQCleaner = {
   getConfig: () => {
-    return ipcRenderer.invoke(EIPCChannel.GET_CONFIG);
+    return ipcRenderer.invoke(EIPCChannel.GET_CONFIG) as Promise<IPluginConfig>;
   },
   setConfig: (config: IPluginConfig) => {
     return ipcRenderer.send(EIPCChannel.SET_CONFIG, config);
   },
 
   getStats: () => {
-    return ipcRenderer.invoke(EIPCChannel.GET_STATS);
+    return ipcRenderer.invoke(EIPCChannel.GET_STATS) as Promise<IPluginStatsExtend>;
   },
 };
 
 contextBridge.exposeInMainWorld('LLQQCleaner', LLQQCleaner);
+export type { LLQQCleaner };
